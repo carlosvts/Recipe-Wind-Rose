e09488a42c3e081f05f3fbd03394707ae2af6663
carlosvtsdev@gmail.com <carlosvtsdev@gmail.com>
2023-07-30 22:32:43 -0300

    Documenting data fetcher module

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -3,11 +3,18 @@ class DataFetcher():
         self.data = data
 
     def extract_recipe_name_and_image(self, data):
+        """
+        Fetch recipe name and image from the api response
+        """
         recipe_name = data[0]["title"]
         recipe_image = data[0]["image"]
         return recipe_name, recipe_image
 
     def extract_used_ingredients(self, data):
+        """
+        Extract from the JSON api response the ingredients that will use for
+        the recipe
+        """
         ingredients_user_have = {}
         for ingredient in data[0]["usedIngredients"]:
             name = ingredient["name"]
@@ -16,6 +23,10 @@ class DataFetcher():
         return ingredients_user_have
 
     def extract_missed_ingredients(self, data):
+        """
+        Extract from the JSON api response the ingredients that are missing
+        for the recipe
+        """
         missed_ingredients = {}
         for ingredient in data[0]["missedIngredients"]:
             name = ingredient["name"]
@@ -24,11 +35,18 @@ class DataFetcher():
         return missed_ingredients
 
     def extract_unused_ingredients(self, data):
+        """
+        Extract from the JSON api response the ingredients that user has
+        passed but he will not use for this recipe
+        """
         unused_ingredients = [ingredient["name"]
                               for ingredient in data[0]["unusedIngredients"]]
         return unused_ingredients
 
     def format_ingredients(self, ingredients):
+        """
+        Iterating trough all lists to get a formated string
+        """
         formatted = ""
         for ingredient, amount in ingredients.items():
             formatted += f"- {ingredient}: {amount}\n"
